a7c8b7aa9456a0c626d6cc268e2326787cd8a32c
sunzid02 <sunzid02@gmail.com>
2020-04-05 19:19:44 +0600

    While loop

--- index.py
+++ index.py
@@ -1,13 +1,22 @@
-print('hi')
+# print('hi')
 
-x = 2
-print(x)
+# x = 2
+# print(x)
 
-x = x + 2
-print(x)
+# x = x + 2
+# print(x)
 
 # Using conditional statement
 if x < 5:
     print('smaller')
 else:
     print('bigger')
+
+
+# While loop
+i = 10
+
+while( i > 0):
+    print(i)
+    i = i-1
+print('Boom')    
