3ad2cf6dcd45df2c2a2c10ba5c5f1794af5fa225
sunzid02 <sunzid02@gmail.com>
2020-04-09 02:07:01 +0600

    input and conversion of numbers

--- index.py
+++ index.py
@@ -7,16 +7,45 @@
 # print(x)
 
 # Using conditional statement
-if x < 5:
-    print('smaller')
-else:
-    print('bigger')
+# x = 2
+
+# if x < 5:
+#     print('smaller')
+# else:
+#     print('bigger')
 
 
-# While loop
-i = 10
+# # While loop
+# # i = 10
 
-while( i > 0):
-    print(i)
-    i = i-1
-print('Boom')    
+# # while( i > 0):
+# #     print(i)
+# #     i = i-1
+# # print('Boom')    
+
+# # array
+# a = [ 10, 20 ]
+
+# i = 0
+# while( i < len(a) ):
+#     print(a[i])
+#     i = i + 1
+
+# print('Boom')    
+
+# take input
+# nam = input('zia');
+# print('welcome Mr.','asdasd', nam);
+
+# europe usa lift problem
+# inp = input('europe floor: ')
+# print('Europe floor', inp)
+
+# usf = int(inp) + 1
+# print('USA floor', usf)
+
+# print("123" + "abc")
+
+Spam = 2
+x = int(98.6)
+print(x)
