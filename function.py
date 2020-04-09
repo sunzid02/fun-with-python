fa0c015311c71d70a0afd590dc9264d45370d2dd
sunzid02 <sunzid02@gmail.com>
2020-04-09 23:23:56 +0600

    assignment 4.6 completed

---
+++ function.py
@@ -0,0 +1,14 @@
+#first function in python
+
+def firstFunction():
+    print('This is my first function')
+
+firstFunction()
+
+
+def stuff():
+    print('Hello')
+    return
+    print('World')
+
+stuff()
