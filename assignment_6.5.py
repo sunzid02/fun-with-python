eb950fc0f224007f65fc916c7bbc0a2250ffc084
sunzid02 <sunzid02@gmail.com>
2020-04-11 17:39:44 +0600

    assignment 6.5 completed

--- assignment_6.5.py
+++ assignment_6.5.py
@@ -1,2 +1,15 @@
 # 6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. 
 # Convert the extracted value to a floating point number and print it out.
+
+# Desired Output
+# 0.8475
+
+text = "X-DSPAM-Confidence:    0.8475";
+
+findColon = text.find(':');
+startPos = text.find('0', findColon);
+
+string = text[ startPos: ];
+result = float(string);
+
+print(string);
