3ea4bf69bf63e4557379db43fb34ba41241e09c7
sunzid02 <sunzid02@gmail.com>
2020-04-30 21:04:25 +0600

    first class oop

--- database/first_class.py
+++ database/first_class.py
@@ -1,10 +1,21 @@
 class PartyAnimal:
     x = 0
+    name = "" 
+
+    def __init__(self, z):
+        self.name = z
+        print(self.name, 'I am constructed')
 
     def party(self):
         self.x = self.x + 1
         print("So far", self.x)
 
-an = PartyAnimal()
+    def __del__(self):
+        print('destructed', self.name)
+
+an = PartyAnimal('zayed')
+an2 = PartyAnimal('zia')
 
 an.party()
+an2.party()
+
